d35796acbee5733312df71b296f2290854643c1e
xiaofengfu <fuxiaofeng1107@163.com>
2017-12-14 12:45:52 +0800

    init

--- sql/pre_sql.sql
+++ sql/pre_sql.sql
@@ -20,12 +20,13 @@ create table nginx_log_report(
   id bigint not null AUTO_INCREMENT comment '主键',
   statistics bigint default 0 comment '总记录数',
   count_time DATETIME DEFAULT now() COMMENT '统计时间',
-  `remote_addr` bigint default 0 comment '远程ip地址',
-  `remote_user` bigint default 0 comment '用户',
-  `status` bigint default 0 comment '状态',
-  `http_user_agent` bigint default 0 comment '用户请求代理'
+  `remote_addr` VARCHAR(100) comment '远程ip地址',
+  `remote_user` VARCHAR(100) comment '用户',
+  `status` VARCHAR(50) comment '状态',
+  `http_user_agent` VARCHAR(500) comment '用户请求代理',
   primary key(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE external TABLE `nginx_log`(
   `remote_addr` string,
