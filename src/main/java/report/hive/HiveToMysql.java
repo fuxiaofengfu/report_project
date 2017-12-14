d6f7cf7bba09acbb69333e9307b4d1311dddd6fd
xiaofengfu <fuxiaofeng1107@163.com>
2017-12-14 11:11:07 +0800

    hive data into mysql

---
+++ src/main/java/report/hive/HiveToMysql.java
@@ -0,0 +1,98 @@
+package report.hive;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import report.jdbc.HiveQuery;
+import report.jdbc.MyTransactionalDML;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class HiveToMysql {
+
+	private final static String HIVE_TABLE_DATESTR="-Ddate_year_month_day";
+
+	public static void main(String[] args) throws SQLException {
+		insertToMysql(args);
+	}
+
+	private static List<Map<String, Object>> getHql(String partitionStr) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("select count(1),remote_user,remote_addr,http_referer ");
+		sql.append("from nginx_log ");
+		if(StringUtils.isNotEmpty(partitionStr)){
+			sql.append("where yearmonth_dir=? and day_dir=? ");
+		}
+		sql.append("group by remote_user,remote_addr,http_referer with cube");
+
+		List<Object> params = null;
+		if(StringUtils.isNotEmpty(partitionStr)){
+			params = new ArrayList<>();
+			String[] split = partitionStr.split("-");
+			params.add(""+split[0]+split[1]);
+			params.add(String.valueOf(split[2]));
+		}
+		System.out.println("执行hql-->>>>>"+sql.toString());
+		List<Map<String, Object>> query = HiveQuery.query(sql.toString(), params);
+		return query;
+	}
+
+	/**
+	 * 需要参数-Ddate_year_month_day=2017-12-12
+	 * @param args
+	 * @throws SQLException
+	 */
+	private static void insertToMysql(String[] args) throws SQLException {
+		//解析分区参数
+		String dateStr = "";
+		for (int i = 0; i < args.length; i++) {
+			if(StringUtils.isNotEmpty(args[i]) &&
+					args[i].contains(HIVE_TABLE_DATESTR)){
+				dateStr = args[i].split("=")[1];
+			}
+		}
+		List<Map<String, Object>> hql = getHql(dateStr);
+		saveToMysql(hql);
+	}
+
+	private static int saveToMysql(List<Map<String, Object>> hiveResult) throws SQLException {
+
+		if(CollectionUtils.isEmpty(hiveResult)){
+			return 0;
+		}
+		StringBuilder builder = new StringBuilder();
+		builder.append("insert into nginx_log_report (");
+		Map<String, Object> columnMap = hiveResult.get(0);
+		int columnIndex = 0;
+		for (String column : columnMap.keySet()) {
+			builder.append(column);
+			columnIndex++;
+			if(columnIndex <= columnMap.keySet().size()-1){
+				builder.append(",");
+			}
+		}
+		builder.append(") values");
+
+		columnIndex = 0;
+		for (int i = 0,j=hiveResult.size(); i < j; i++) {
+			builder.append("(");
+			Map<String,Object> valueMap = hiveResult.get(i);
+			for(Object object : valueMap.values()){
+				builder.append(object);
+				if(columnIndex<=valueMap.values().size() -2 ){
+					builder.append(",");
+				}
+				columnIndex++;
+			}
+			builder.append(")");
+			if(i<=j-2){
+				builder.append(",");
+			}
+		}
+		System.out.println("执行mysql-->>>>>>"+builder.toString());
+		return  MyTransactionalDML.executeDML(builder.toString(), null);
+	}
+}
---
+++ src/main/java/report/jdbc/HiveQuery.java
@@ -0,0 +1,44 @@
+package report.jdbc;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.hive.jdbc.HivePreparedStatement;
+import org.apache.hive.jdbc.HiveQueryResultSet;
+import report.jdbc.hive.HiveConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HiveQuery {
+
+	public static List<Map<String,Object>> query(String hql,List<Object> params) throws SQLException {
+		List<Map<String,Object>> data;
+		try {
+			Connection hiveConnection = HiveConnection.getHiveConnection();
+			PreparedStatement preparedStatement = hiveConnection.prepareStatement(hql);
+			if(CollectionUtils.isNotEmpty(params)){
+				for (int i = 0; i < params.size(); i++) {
+					int pindex = i + 1;
+					preparedStatement.setObject(pindex,params.get(i));
+				}
+			}
+			ResultSet resultSet = preparedStatement.executeQuery();
+			ResultSetMetaData metaData = resultSet.getMetaData();
+			int columnCount = metaData.getColumnCount();
+			data = new ArrayList<Map<String,Object>>(100);
+			while (resultSet.next()){
+				HashMap<String, Object> hashMap = new HashMap<>();
+				for (int i = 1; i < columnCount; i++) {
+					hashMap.put(metaData.getColumnName(i),resultSet.getObject(i));
+				}
+				data.add(hashMap);
+			}
+		} finally {
+			HiveConnection.closeHiveConnect();
+		}
+		return data;
+	}
+
+}
