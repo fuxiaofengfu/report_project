e5d32eb184a821a924729c4cd91841473f42bde7
xiaofengfu <fuxiaofeng1107@163.com>
2017-12-14 13:08:27 +0800

    add countHiveToMysql

--- src/main/java/report/mr/JobDriverManager.java
+++ src/main/java/report/mr/JobDriverManager.java
@@ -1,6 +1,7 @@
 package report.mr;
 
 import org.apache.hadoop.util.ProgramDriver;
+import report.hive.HiveToMysql;
 import report.mr.jobcontrol.JobControlRun;
 
 public class JobDriverManager {
@@ -9,6 +10,7 @@ public class JobDriverManager {
 		ProgramDriver programDriver = new ProgramDriver();
 		try {
 			programDriver.addClass("avroToOrc", JobControlRun.class,"avro格式转orc格式输出");
+			programDriver.addClass("countHiveToMysql", HiveToMysql.class,"将hive数据表统计到mysql表中");
 			programDriver.run(args);
 		} catch (Throwable throwable) {
 			throwable.printStackTrace();
